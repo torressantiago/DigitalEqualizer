a99f3f49b034211cda64a75e157f54f43bfb7cff
Santiago Torres <torres_santiago@javeriana.edu.co>
2019-10-26 13:25:58 -0500

    Update SerialMonitor.py

--- SerialMonitor.py
+++ SerialMonitor.py
@@ -1,10 +1,25 @@
 import serial
 from time import sleep
-import sys
+import matplotlib.pyplot as plt
+from drawnow import *
+# import numpy as np
+
+Res = []
+plt.ion() # Interactive mode to plot live data
+i = 0
+
+def makeFig():
+	plt.ylim(-1,1) #Set y min and max values
+	plt.title('Serial plotter') #Plot the title
+	plt.grid(True) #Turn the grid on
+	plt.ylabel('Decoded value') #Set ylabels
+	plt.plot(Res, 'ro-', label='Received value') #plot the temperature
+	plt.legend(loc='upper left') #plot the legend
+	plt.draw()
 
 
-COM = 'COM3'# /dev/ttyACM0 (Linux)
-BAUD = 128000
+COM = 'COM10'# /dev/ttyACM0 (Linux)
+BAUD = 9600
 
 ser = serial.Serial(COM, BAUD, timeout = .1)
 
@@ -12,13 +27,16 @@ print('Waiting for device');
 sleep(3)
 print(ser.name)
 
-#check args
-monitor = True
-
 
 while True:
-	val = str(ser.readline().decode().strip('\r\n'))#Capture serial output as a decoded string
-	valA = val.split("/")
-	#print()
-	if(monitor == True):
-		print(val, end="\r\n", flush=True)
+    #while (ser.inWaiting == 0):
+       # pass # while there is no data incoming, do nothing
+	
+	ResString = str(ser.readline().decode().strip('\r'))#Capture serial output as a decoded string
+	ResNum = float(ResString)
+	print(ResString)
+	Res.append(ResNum)
+	drawnow(makeFig)
+	i = i + 1
+#	if(i > 15):                          
+#		Res.pop(0) # This allows to just see the last 15 data points
