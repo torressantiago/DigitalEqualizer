1d9771acfe18905d07ce96dea92e661d800d211e
Santiago Torres <torres_santiago@javeriana.edu.co>
2019-10-26 19:59:15 -0500

    Update SerialMonitor.py

--- SerialMonitor.py
+++ SerialMonitor.py
@@ -1,49 +1,53 @@
+####################### Library definitions ###################################
 import serial
 from time import sleep
 import matplotlib.pyplot as plt
 from drawnow import *
 import numpy as np
 
+###################### Variable declarations ##################################
 Res = []
-ResNumL = [[]]
+ResNumL = np.ones((15,3))
+ResAvg = np.asarray([])
 plt.ion() # Interactive mode to plot live data
+column = 0
+row = 0
 i = 0
-x = np.linspace(1,15,15)
+x = np.linspace(0,14,15)
 
+COM = 'COM10'# /dev/ttyACM0 (Linux)
+BAUD = 2400
+
+ser = serial.Serial(COM, BAUD, timeout = 0.1)
+
+###################### Function definitions ###################################
 def makeFig():
 	plt.ylim(-1,1) #Set y min and max values
 	plt.title('Serial plotter') #Plot the title
 	plt.grid(True) #Turn the grid on
 	plt.ylabel('Decoded value') #Set ylabels
-	plt.plot(Res, 'ro', label='Received value') #plot the temperature
+	plt.plot(ResAvg, 'ro', label='Received value') #plot the received value
 	plt.legend(loc='upper left') #plot the legend
 	plt.draw()
 
 
-
-COM = 'COM10'# /dev/ttyACM0 (Linux)
-BAUD = 2000000
-
-ser = serial.Serial(COM, BAUD, timeout = .1)
-
+###################### Program flow ###########################################
 print('Waiting for device');
 sleep(3)
 print(ser.name)
 
 for BigCounter in range(3):
+    print(BigCounter)
+    column = int(BigCounter)
     for counter in x:
-        ResString = str(ser.readline().decode().strip('\r'))#Capture serial output as a decoded string
+        print(counter)
+        row = int(counter)
+        ResString = str(ser.readline().decode().strip('\r\n')) # Capture serial output as a decoded string
         ResNum = float(ResString)
-        print(ResString)
         Res.append(ResNum)
-        drawnow(makeFig)
-    #	i = i + 1
-    #	if(i > 15):                          
-    #		Res.pop(0) # This allows to just see the last 15 data points
-#    plt.ylim(-1,1) #Set y min and max values
-#    plt.title('Serial plotter') #Plot the title
-#    plt.grid(True) #Turn the grid on
-#    plt.ylabel('Decoded value') #Set ylabels
-#    plt.plot(x,Res, label='Received value') #plot the temperature
-#    plt.legend(loc='upper left') #plot the legend
-#    plt.draw()
+        ResNumL[row][column] = np.asarray(ResNum)
+        
+    ResAvg = np.mean(ResNumL,axis = 1)
+    drawnow(makeFig)
+        
+ser = None # Closes port
