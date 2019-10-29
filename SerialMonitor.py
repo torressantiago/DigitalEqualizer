c5546a86c5e4bbc64e24abca1c93aa4815e34c60
Santiago Torres <torres_santiago@javeriana.edu.co>
2019-10-28 19:24:26 -0500

    Update SerialMonitor.py

--- SerialMonitor.py
+++ SerialMonitor.py
@@ -7,7 +7,6 @@ import numpy as np
 
 ###################### Variable declarations ##################################
 Res = []
-ResNumL = np.ones((15,3))
 ResAvg = np.asarray([])
 plt.ion() # Interactive mode to plot live data
 column = 0
@@ -34,7 +33,7 @@ def makeFig():
 	plt.ylabel('Decoded value') #Set ylabels
 	plt.plot(ResAvg, 'ro-', label='Received value') #plot the received value
 	plt.legend(loc='upper left') #plot the legend
-	plt.show(block = True)
+	plt.show(block = False)
 
 
 ###################### Program flow ###########################################
@@ -43,55 +42,63 @@ sleep(3)
 print(ser.name)
 print(BAUD)
 
-if state == 0:
-    print('-------------Main Menu-------------')
-    print('Press the number of the option wanted')
-    print('1. Send data')
-    print('2. Receive data')
-    print('3. Help')
-    
-    state = input()
-
-if state == 1:
-    print("Input the value you want to send")
-    inputvar = input()
-    inputvar += "\r\n"
-    ser.write(inputvar.encode())
+while state >= 0:
+    if state == 0:
+        print('-------------Main Menu-------------')
+        print('Press the number of the option wanted')
+        print('1. Send data')
+        print('2. Receive data')
+        print('3. Help')
+        print('4. Quit')
         
-    ser = None # Closes port   
-    print('Returning to Main Menu...')
-    state = 0
-    
-if state == 2:
-    print("Input number of iterations and window size")
-    Iterations = input()
-    Window = input()
-    for BigCounter in range(Iterations):
-        print(BigCounter)
-        column = int(BigCounter)
-        for counter in range(Window):
-            print(counter)
-            row = int(counter)
-            ResString = str(ser.readline().decode().strip('\r\n')) # Capture serial output as a decoded string
-            ResNum = float(ResString)
-            Res.append(ResNum)
-            ResNumL[row][column] = np.asarray(ResNum)
+        state = int(input())
+        
+    elif state == 1:
+        print("Input the value you want to send")
+        inputvar = input()
+        inputvar += "\r\n"
+        ser.write(inputvar.encode())
             
-        ResAvg = np.mean(ResNumL,axis = 1)
+        ser = None # Closes port   
+        print('Returning to Main Menu...')
+        state = 0
         
+    elif state == 2:
+        print("Input number of iterations and window size")
+        Iterations = int(input())
+        Window = int(input())
+        ResNumL = np.ones((Window,Iterations))
+        for BigCounter in range(Iterations):
+            print(BigCounter)
+            column = int(BigCounter)
+            for counter in range(Window):
+                print(counter)
+                row = int(counter)
+                ResString = str(ser.readline().decode().strip('\r\n')) # Capture serial output as a decoded string
+                ResNum = float(ResString)
+                print(ResNum)
+                Res.append(ResNum)
+                ResNumL[row][column] = np.asarray(ResNum)
+                
+            ResAvg = np.mean(ResNumL,axis = 1)
+            
         drawnow(makeFig) # plot average after window
-            
-    ser = None # Closes port
-    print('Returning to Main Menu...')
-    state = 0
+                
+        ser = None # Closes port
+        print('Returning to Main Menu...')
+        state = 0
+        
+    elif state == 3:
+        print('-------------Help-------------')
+        print('This program can receive/send at a certain baudrate information received using serial protocol')
+        print('To change baudrate and port, please change BAUD and COM respectively in code')
+        print('Returning to Main Menu...')
+        state = 0
     
-if state == 3:
-    print('-------------Help-------------')
-    print('This program can receive/send at a certain baudrate information received using serial protocol')
-    print('To change baudrate and port, please change BAUD and COM respectively in code')
-    print('Returning to Main Menu...')
-    state = 0
-
-else:
-    print('Command not recognized')
-    print('Returning to Main Menu...')
+    elif state == 4:
+        state = -1
+        ser = None # Closes port
+    
+    else:
+        print('Command not recognized')
+        print('Returning to Main Menu...')
