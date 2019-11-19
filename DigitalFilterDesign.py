c4b0aa6cab9af37fe94fe0004885be4ca7a09760
Santiago Torres <torres_santiago@javeriana.edu.co>
2019-11-19 16:29:15 -0500

    Update DigitalFilterDesign.py

--- DigitalFilterDesign.py
+++ DigitalFilterDesign.py
@@ -1,19 +1,49 @@
-# Digital filter design
-import numpy as np
-from scipy import signal
+from scipy.signal import butter, freqz
 import matplotlib.pyplot as plt
+from math import pi
+import numpy as np
 
-# Butterworth lowpass, fs = 48000, order 4
-a,b = signal.iirfilter(4, [200], btype='lp',
-                       analog=False, ftype='butter', fs=48000,
-                       output='ba')
-w, h = signal.sosfreqz(sos, 2000, fs=2000)
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-ax.semilogx(w, 20 * np.log10(np.maximum(abs(h), 1e-5)))
-ax.set_title('Chebyshev Type II bandpass frequency response')
-ax.set_xlabel('Frequency [Hz]')
-ax.set_ylabel('Amplitude [dB]')
-ax.axis((0, 1000, -100, 10))
-ax.grid(which='both', axis='both')
+f_s = 48000    # Sample frequency in Hz
+f_c = 1000     # Cut-off frequency in Hz
+order = 4    # Order of the butterworth filter
+
+omega_c = 2 * pi * f_c       # Cut-off angular frequency
+omega_c_d = omega_c / f_s    # Normalized cut-off frequency (digital)
+
+# Design the digital Butterworth filter
+b, a = butter(order, omega_c_d / pi, btype = 'lowpass') 
+b = b * 6   
+print('Coefficients')
+print("b =", b)                           # Print the coefficients
+print("a =", a)
+
+w, H = freqz(b, a, 4096)                  # Calculate the frequency response
+w *= f_s / (2 * pi)                       # Convert from rad/sample to Hz
+
+# Plot the amplitude response
+plt.subplot(2, 1, 1)            
+plt.suptitle('Bode Plot')
+H_dB = 20 * np.log10(abs(H))              # Convert modulus of H to dB
+plt.plot(w, H_dB)
+plt.ylabel('Magnitude [dB]')
+plt.xlim(0, f_s / 2)
+plt.ylim(-80, 40)
+plt.axvline(f_c, color='red')
+plt.grid()
+plt.axhline(-3, linewidth=0.8, color='black', linestyle=':')
+
+# Plot the phase response
+plt.subplot(2, 1, 2)
+phi = np.angle(H)                         # Argument of H
+phi = np.unwrap(phi)                      # Remove discontinuities 
+phi *= 180 / pi                           # and convert to degrees
+plt.plot(w, phi)
+plt.xlabel('Frequency [Hz]')
+plt.ylabel('Phase [°]')
+plt.xlim(0, f_s / 2)
+plt.ylim(-360, 0)
+plt.yticks([-360, -270, -180, -90, 0])
+plt.grid()
+plt.axvline(f_c, color='red')
+
 plt.show()
