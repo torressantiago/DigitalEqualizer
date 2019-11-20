de20dbbe84813eaa0761eff164ad37d57d52aaf7
Santiago Torres <torres_santiago@javeriana.edu.co>
2019-11-19 21:08:25 -0500

    Update SerialMonitor.py

--- SerialMonitor.py
+++ SerialMonitor.py
@@ -59,27 +59,6 @@ def filterDesign():
     print("b = ",b)
     return b
 
-def getExpandedScientificNotation(flt):
-    was_neg = False
-    if not ("e" in flt):
-        return flt
-    if flt.startswith('-'):
-        flt = flt[1:]
-        was_neg = True 
-    str_vals = str(flt).split('e')
-    coef = float(str_vals[0])
-    exp = int(str_vals[1])
-    return_val = ''
-    if int(exp) > 0:
-        return_val += str(coef).replace('.', '')
-        return_val += ''.join(['0' for _ in range(0, abs(exp - len(str(coef).split('.')[1])))])
-    elif int(exp) < 0:
-        return_val += '0.'
-        return_val += ''.join(['0' for _ in range(0, abs(exp) - 1)])
-        return_val += str(coef).replace('.', '')
-    if was_neg:
-        return_val='-'+return_val
-    return return_val
 ###################### Program flow ###########################################
 print('Waiting for device');
 sleep(3)
