de23319c6f94f7d05ee7f32358f8d1897947ef63
Santiago Torres <torres_santiago@javeriana.edu.co>
2019-11-20 14:50:02 -0500

    Update SerialMonitor.py

--- SerialMonitor.py
+++ SerialMonitor.py
@@ -58,6 +58,22 @@ def filterDesign():
     b = signal.firwin2(Order, [((0)*2/f_s),(f[0]),(f[1]),(f[2]),((24000*2)/f_s)],[0,g[0],g[1],g[2],0])
     print("Coefficients")
     print("b = ",b)
+    w, h = signal.freqz(b)
+    w *= f_s / (2 * pi)                       # Convert from rad/sample to Hz
+    
+    import matplotlib.pyplot as plt
+    fig, ax1 = plt.subplots()
+    ax1.set_title('Digital filter frequency response')
+    ax1.plot(w, 20 * np.log10(abs(h)), 'b')
+    ax1.set_ylabel('Amplitude [dB]', color='b')
+    ax1.set_xlabel('Frequency [Hz]')
+    ax2 = ax1.twinx()
+    angles = np.unwrap(np.angle(h))
+    ax2.plot(w, angles, 'g')
+    ax2.set_ylabel('Angle (radians)', color='g')
+    ax2.grid()
+    ax2.axis('tight')
+    plt.show()
     return b
 
 ###################### Program flow ###########################################
